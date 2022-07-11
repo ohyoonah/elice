a2ebc840d48c73e53609d8241a3321cc50b204a1
ohyoonah <dhdbsdk3@naver.com>
2022-07-11 11:14:41 +0900

    dom 실습

---
+++ 05/js/dom.js
@@ -0,0 +1,21 @@
+// const chicken = document.querySelector('p');
+// //괄호 안에는 tag 작성
+// console.log(chicken);
+// //<p>후참</p>
+// //맨 처음의 태그를 인식해서 가져온다.
+
+// const bestchicken = document.querySelector('.best');
+// console.log(bestchicken);
+// //<p class="best">황금올리브</p>
+
+// const para = document.querySelector('body > h3');
+// console.log(para);
+
+// const checkall = document.querySelectorAll('p');
+// console.log(checkall);
+// //모든 p태그가 배열로 담긴다.
+
+const chicken = document.getElementById('best');
+console.log(chicken);
+const bestchicken = document.getElementsByClassName('best');
+console.log(bestchicken);
