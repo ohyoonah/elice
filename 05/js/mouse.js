218b50a77bf1419d30f67c064a24a6f206e79d9d
ohyoonah <dhdbsdk3@naver.com>
2022-07-11 14:22:09 +0900

    js 마우스 이벤트 실습

---
+++ 05/js/mouse.js
@@ -0,0 +1,5 @@
+const box = document.querySelector('.box');
+box.addEventListener('mousemove', e => {
+  console.log(e.offsetX, e.offsetY);
+  box.textContent = `x pos : ${e.offsetX} y pos : ${e.offsetY}`;
+});
